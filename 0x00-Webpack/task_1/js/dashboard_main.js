e4a68c0cd0ca8458bd310b9ed8d1e59338e54b03
Yator1 <titusyatz17@gmail.com>
2024-11-10 11:07:54 +0300

    using webpack with config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -6,14 +6,16 @@ let count = 0;
 function updateCounter() {
     count += 1;
     $('#count').text(`${count} clicks on the button`);
+    console.log(`counter updated to ${count}`);
 }
 
 $(function() {
+    console.log('Document is ready');
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
     $('body').append('<button>Click here to get started</button>');
     $('body').append('<p id="count"></p>');
     $('body').append('<p>Copyright - Holberton School</p>');
 
-    $('#start-button').on('click', _.debounce(updateCounter, 500));
+    $('button').on('click', _.debounce(updateCounter, 500));
 });
