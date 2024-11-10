84db6dd5e89051551be1ebd114012fd950d8e45d
Yator1 <titusyatz17@gmail.com>
2024-11-10 12:57:30 +0300

    adding css and images

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+
+let count = 0;
+
+function updateCounter() {
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+    console.log(`counter updated to ${count}`);
+}
+
+$(function() {
+    console.log('Document is ready');
+    $('body').prepend('<div id="logo"></div>');
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button>Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+    $('button').on('click', _.debounce(updateCounter, 500));
+});
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // Add the clean-webpack-plugin
+
+module.exports = {
+  entry: './js/dashboard_main.js',  // Single entry point
+  output: {
+    filename: 'bundle.js', // Ensure the single output file
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'production',  // Production mode
+  performance: {
+    hints: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader'], // Load CSS
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',  // Handle image assets
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: process.env.NODE_ENV !== 'production', // Disable in non-production mode
+              mozjpeg: {
+                progressive: true,
+                quality: 65,  // Set a specific quality for JPEGs
+              },
+              optipng: {
+                enabled: true,
+                optimizationLevel: 7, // Optimize PNGs
+              },
+              pngquant: {
+                quality: [0.6, 0.8],  // Optimize PNGs further
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              svgo: {
+                plugins: [
+                  { removeViewBox: false },
+                  { cleanupIDs: false },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: false,
+  },
+//   plugins: [
+//     new CleanWebpackPlugin(), // Clean the public folder before build
+//   ],
+};
