2abdfa16097e0c63321f9a8d4e15e70969d146e9
Yator1 <titusyatz17@gmail.com>
2024-11-13 10:57:45 +0400

    webpack dev

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',  // Single entry point
+  output: {
+    filename: 'bundle.js', // Ensure the single output file
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'production',  // Production mode
+  performance: {
+    hints: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader'], // Load CSS
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',  // Handle image assets
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: process.env.NODE_ENV !== 'production', // Disable in non-production mode
+              mozjpeg: {
+                progressive: true,
+                quality: 65,  // Set a specific quality for JPEGs
+              },
+              optipng: {
+                enabled: true,
+                optimizationLevel: 7, // Optimize PNGs
+              },
+              pngquant: {
+                quality: [0.6, 0.8],  // Optimize PNGs further
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              svgo: {
+                plugins: [
+                  { removeViewBox: false },
+                  { cleanupIDs: false },
+                ],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i, // Match font files
+        use: [
+          {
+            loader: 'file-loader', // Use file-loader for fonts
+            options: {
+              name: '[name].[hash].[ext]', // File naming pattern
+              outputPath: 'fonts/', // Output folder for fonts
+            },
+          },
+        ],
+      }
+    ],
+  },
+  optimization: {
+    splitChunks: false,
+  },
+};
